0eb5fb0a2f615cbd51aafe3ed1d49ad212338fee
admin <konstantin.duvakin-87@yandex.ru>
2022-05-10 22:34:55 +0500

    hook up sanity

---
+++ components/index.js
@@ -0,0 +1,8 @@
+export {HeroBanner} from "./HeroBanner";
+export {FooterBanner} from "./FooterBanner";
+export {Footer} from "./Footer";
+export {Cart} from "./Cart";
+export {Product} from "./Product";
+export {Layout} from "./Layout";
+export {Navbar} from "./Navbar";
+
